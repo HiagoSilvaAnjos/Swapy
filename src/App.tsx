47ba67e87ef76799ed3382b0217efdd48e123303
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-09-25 22:46:34 -0300

    feat: add initial setup swapy

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { createSwapy } from "swapy";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const container = document.querySelector(".container");
+
+    if (container) {
+      const swapy = createSwapy(container, {
+        animation: "dynamic", // or spring or none
+      });
+
+      swapy.onSwap((event) => {
+        console.log(event.data.object);
+        console.log(event.data.array);
+        console.log(event.data.map);
+      });
+    }
+  }, []); // O array vazio [] garante que o código será executado apenas após o primeiro render.
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="section-1" data-swapy-slot="text-pink">
+          <div className="content-a" data-swapy-item="a">
+            <div className="handle" data-swapy-handle></div>
+            <h3>title</h3>
+            <p>
+              {" "}
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus quaerat repellat rem fugiat accusamus quo ex
+              similique, id consequuntur impedit adipisci earum laboriosam odit
+              reiciendis quidem, quam magni dolorem expedita.
+            </p>
+          </div>
+        </div>
+
+        <div className="section-2" data-swapy-slot="text-blue">
+          <div className="content-b" data-swapy-item="b">
+            <div className="handle" data-swapy-handle></div>
+            <h3>title</h3>
+            <p>
+              {" "}
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus quaerat repellat rem fugiat accusamus quo ex
+              similique, id consequuntur impedit adipisci earum laboriosam odit
+              reiciendis quidem, quam magni dolorem expedita.
+            </p>
+          </div>
+        </div>
+
+        <div className="section-3" data-swapy-slot="text-green">
+          <div className="content-c" data-swapy-item="c">
+            <div className="handle" data-swapy-handle></div>
+            <h3>title</h3>
+            <p>
+              {" "}
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus quaerat repellat rem fugiat accusamus quo ex
+              similique, id consequuntur impedit adipisci earum laboriosam odit
+              reiciendis quidem, quam magni dolorem expedita.
+            </p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
